e1132cc41b56ee9e76dc1fa7f6c785b4bc42a578
Tierney Cyren <hello@bnb.im>
2022-02-01 15:04:38 -0500

    feat: improve the code
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- report.js
+++ report.js
@@ -1,41 +1,41 @@
 const { getPublicGitHubEvents, filterPublicGitHubEvents, getEventsFrom, writeEventsFile, writeMarkdownFile, writeMegafile } = require('encites')
-const dates = require('./lib/dates')
+const users = require('./util/people')
+const dates = require('./util/dates')
+const createPaths = require('./util/paths')
+const createHeadings = require('./util/headings')
+const { unlink } = require('fs/promises')
+
+const paths = createPaths(dates)
+const headings = createHeadings(dates)
 
 async function teamData () {
-  const paths = {
-    data: './output/data/', // path to write all our files to.
-    raw: './output/raw/', // path to write raw GitHub data to - this allows us to consume it later as we add more events to output
-    markdownOutputDir: './output/' // the path that we'll write our markdown files to
-  }
-
-  const filenames = {
-    megafile: 'megafile.json', // name of the megafile. can be whatever, I've just chosen megafile. Needs to be JSON.
-    markdown: 'report.md' // name of the markdown file we're going to write. Needs to be Markdown.
-  }
-
-  // GitHub usernames we're going to collect public GitHub API data for
-  const users = ['bnb']
-
   // fetch data from GitHub
   const rawData = await getPublicGitHubEvents(users)
   // fetches public data from  the GitHub API
   const data = await filterPublicGitHubEvents(rawData)
 
-  // save our publicData for future use
-  writeEventsFile(`${paths.raw}`, rawData)
-  // attempt to save our data as a Megafile
-  writeMegafile(`${paths.raw}`, filenames.megafile)
+  // // save our publicData for future use
+  await writeEventsFile(`${paths.raw}`, rawData)
+
+  // attempt to save our data as a Megafile?
+  await writeMegafile(`${paths.raw}`, paths.filenames.megafile)
 
   // write single instance of data
-  writeEventsFile(`${paths.data}`, data)
+  await writeEventsFile(`${paths.data}`, data)
   // write the megafile
-  writeMegafile(`${paths.data}`, filenames.megafile)
+  await writeMegafile(`${paths.data}`, paths.filenames.megafile)
 
   // gets all data locally from the megafile, filtering out events *before* the date passed
-  const dateFilteredEvents = await getEventsFrom.period(`${paths.data}${filenames.megafile}`, dates.checkFrom, dates.checkUntil)
+  const dateFilteredEvents = await getEventsFrom.period(`${paths.data}${paths.filenames.megafile}`, paths.dateToCheck.from, paths.dateToCheck.until)
 
   // write the markdown to the correct location
-  writeMarkdownFile(paths.markdownOutputDir, filenames.markdown, dateFilteredEvents, `Weekly Open Source Report: ${dates.now.toISODate()}`)
+  writeMarkdownFile(paths.reports.weekly, paths.filenames.weekly, dateFilteredEvents, headings.weekly)
+
+  // delete the megafiles because lol they can get over 100mb and GitHub fails to run Actions because of that
+  const deleteRawMegafileForGitHubSizeConstraints = await unlink(`${paths.raw}${paths.filenames.megafile}`)
+  const deleteFilteredMegafileForGitHubSizeConstraints = await unlink(`${paths.data}${paths.filenames.megafile}`)
+
+  console.log(deleteRawMegafileForGitHubSizeConstraints, deleteFilteredMegafileForGitHubSizeConstraints)
 }
 
 teamData()
